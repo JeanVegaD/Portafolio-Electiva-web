df5eabd1879eefe68d5a459710e5cd7fa8778c58
JeanVegaD <jean0798@gmail.com>
2019-12-09 23:32:21 -0600

    Modulo asignaciones

--- logica.js
+++ logica.js
@@ -16,7 +16,7 @@ function cerrarModal_curso(){
 }
 
 
-
+//Modal Sobre mi 
 function abrirModal_sobremi(){
     // Obtiene el modal
     var modal = document.getElementById("modal_sobremi");
@@ -33,3 +33,21 @@ function cerrarModal_sobremi(){
     modal.style.display = "none";
 }
 
+
+//Modal Asignaciones
+
+function abrirModal_asignaciones(){
+    // Obtiene el modal
+    var modal = document.getElementById("modal_asignaciones");
+    //Muestra el modal
+    modal.style.display = "block";
+}
+
+
+function cerrarModal_asignaciones(){
+    // obtiene el tag span
+    var modal = document.getElementById("modal_asignaciones");
+    var span = document.getElementsByClassName("close")[0];
+    // When the user clicks the button, open the modal 
+    modal.style.display = "none";
+}
