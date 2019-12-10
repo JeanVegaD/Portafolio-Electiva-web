c899bc95b8e91b244754769bc851f01afee44dcf
JeanVegaD <jean0798@gmail.com>
2019-12-09 22:29:27 -0600

    Se agrego estilos, secciones de curso y sobre mi

---
+++ logica.js
@@ -0,0 +1,35 @@
+
+function abrirModal_curso(){
+    // Obtiene el modal
+    var modal = document.getElementById("modal_curso");
+    //Muestra el modal
+    modal.style.display = "block";
+
+}
+
+function cerrarModal_curso(){
+    // obtiene el tag span
+    var modal = document.getElementById("modal_curso");
+    var span = document.getElementsByClassName("close")[0];
+    // When the user clicks the button, open the modal 
+    modal.style.display = "none";
+}
+
+
+
+function abrirModal_sobremi(){
+    // Obtiene el modal
+    var modal = document.getElementById("modal_sobremi");
+    //Muestra el modal
+    modal.style.display = "block";
+
+}
+
+function cerrarModal_sobremi(){
+    // obtiene el tag span
+    var modal = document.getElementById("modal_sobremi");
+    var span = document.getElementsByClassName("close")[0];
+    // When the user clicks the button, open the modal 
+    modal.style.display = "none";
+}
+
